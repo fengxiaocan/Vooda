02e9eb67b4fe77cead36d5275e53293533f783d2
fengxiaocan <fengxiaocan@gmail.com>
2018-01-31 10:42:12 +0800

    修正时间问题

--- checkouttime/src/main/java/com/evil/check/CheckOutTime.java
+++ checkouttime/src/main/java/com/evil/check/CheckOutTime.java
@@ -17,35 +17,35 @@ import java.util.Date;
  * @描述： 检测是否超时
  */
 
-public final class CheckOutTime{
+public final class CheckOutTime {
 
-    public static final  long   DAY_TIME  = 1000 * 60 * 60 * 24;
-    public static final  String webUrl1   = "http://www.bjtime.cn";//bjTime
-    public static final  String webUrl2   = "http://www.baidu.com";//百度
-    public static final  String webUrl3   = "http://www.taobao.com";//淘宝
-    public static final  String webUrl4   = "http://www.ntsc.ac.cn";
+    public static final long DAY_TIME = 1000 * 60 * 60 * 24;
+    public static final String webUrl1 = "http://www.bjtime.cn";//bjTime
+    public static final String webUrl2 = "http://www.baidu.com";//百度
+    public static final String webUrl3 = "http://www.taobao.com";//淘宝
+    public static final String webUrl4 = "http://www.ntsc.ac.cn";
     //中国科学院国家授时中心
-    public static final  String webUrl5   = "http://www.360.cn";//360
-    public static final  String webUrl6   = "http://www.beijing-time.org";
+    public static final String webUrl5 = "http://www.360.cn";//360
+    public static final String webUrl6 = "http://www.beijing-time.org";
     //beijing-time
-    protected static final String sName     = "CheckOutTime";
+    protected static final String sName = "CheckOutTime";
     protected static final String sCreateTimeName = "CreateTime";
     protected static final String sNewOutTimeName = "NewOutTime";
 
-    static long[] getNewTime(){
+    static long[] getNewTime() {
         //格式为:包名&$&生成的时间戳&$&校验过期时间&$&最终的过期时间&$&校验码
         //校验码为生成的时间戳%100+校验过期时间%20
         SharedPreferences sp = getContext()
-                .getSharedPreferences(sName,Context.MODE_PRIVATE);
-        long createTime = sp.getLong(sCreateTimeName,0);
-        long newTime = sp.getLong(sNewOutTimeName,0);
-        if(createTime != 0 && newTime != 0){
-            return new long[]{createTime,newTime};
+                .getSharedPreferences(sName, Context.MODE_PRIVATE);
+        long createTime = sp.getLong(sCreateTimeName, 0);
+        long newTime = sp.getLong(sNewOutTimeName, 0);
+        if (createTime != 0 && newTime != 0) {
+            return new long[]{createTime, newTime};
         }
         return null;
     }
 
-    static Context getContext(){
+    static Context getContext() {
         return ApplicationLoader.get().getBaseContext();
     }
 
@@ -53,14 +53,12 @@ public final class CheckOutTime{
      * 获取指定网站的日期时间
      *
      * @param webUrl
-     *
      * @return
-     *
      * @author SHANHY
      * @date 2015年11月27日
      */
-    private static long getWebsiteDatetime(String webUrl){
-        try{
+    public static long getWebsiteDatetime(String webUrl) {
+        try {
             URL url = new URL(webUrl);// 取得资源对象
             HttpURLConnection uc = (HttpURLConnection) url
                     .openConnection();// 生成连接对象
@@ -68,9 +66,12 @@ public final class CheckOutTime{
             long ld = uc.getDate();// 读取网站日期时间
             Date date = new Date(ld);// 转换为标准时间对象
             long time = date.getTime();
+            if (time <= 1000) {
+                time = System.currentTimeMillis();
+            }
             uc.disconnect();
             return time;
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return System.currentTimeMillis();
@@ -79,45 +80,43 @@ public final class CheckOutTime{
     /**
      * 检测时间是否过期
      *
-     * @param outTime
-     *         时间,以毫秒为单位,1000L为去1秒;
-     * @param callback
-     *         结果回调,如果为true,则finish所有Activity
+     * @param outTime  时间,以毫秒为单位,1000L为去1秒;
+     * @param callback 结果回调,如果为true,则finish所有Activity
      */
     public static void checkTime(final long outTime,
-                                 final TimeOutCallback callback){
-        new Thread(new Runnable(){
+                                 final TimeOutCallback callback) {
+        new Thread(new Runnable() {
             @Override
-            public void run(){
+            public void run() {
                 //按照版本构建的时间为测试版起始点
                 String buildStr = ManifestUtils
-                        .getManifestString(getContext(),"BUILD_TIME");
+                        .getManifestString(getContext(), "BUILD_TIME");
                 String time = buildStr.substring(4);
                 //获取当前时间
                 long currentTime = getWebsiteDatetime(webUrl4);
-                Log.e("checkTime","currentTime="+currentTime);
+                Log.e("checkTime", "currentTime=" + currentTime);
                 //获取创建时间
                 long buildTime = Long.valueOf(time);
-                Log.e("checkTime","buildTime="+buildTime);
+                Log.e("checkTime", "buildTime=" + buildTime);
                 //获取过期时间
                 long newOutTime = buildTime + outTime;
                 //获取最新的测试码时间
                 long[] newTime = getNewTime();
 
-                if(newTime != null){
+                if (newTime != null) {
                     //比较创建时间
-                    if(newTime[0] > buildTime){
+                    if (newTime[0] > buildTime) {
                         buildTime = newTime[0];
                     }
 
-                    if(newTime[1] > newOutTime){
+                    if (newTime[1] > newOutTime) {
                         newOutTime = newTime[1];
                     }
                 }
-                if(currentTime > newOutTime || currentTime < buildTime){
+                if (currentTime > newOutTime || currentTime < buildTime) {
                     //超过有效期
                     callback.method(true);
-                }else{
+                } else {
                     callback.method(false);
                 }
             }
@@ -127,53 +126,51 @@ public final class CheckOutTime{
     /**
      * 检测时间是否过期
      *
-     * @param outDay
-     *         时间,以天为单位,1为去1天;
-     * @param callback
-     *         结果回调,如果为true,则finish所有Activity
+     * @param outDay   时间,以天为单位,1为去1天;
+     * @param callback 结果回调,如果为true,则finish所有Activity
      */
     public static void checkTime(final int outDay,
-                                 final TimeOutCallback callback){
-        new Thread(new Runnable(){
+                                 final TimeOutCallback callback) {
+        new Thread(new Runnable() {
             @Override
-            public void run(){
+            public void run() {
                 //按照版本构建的时间为测试版起始点
                 String buildStr = ManifestUtils
-                        .getManifestString(getContext(),"BUILD_TIME");
+                        .getManifestString(getContext(), "BUILD_TIME");
                 String time = buildStr.substring(4);
                 //获取当前时间
                 long currentTime = getWebsiteDatetime(webUrl4);
-                Log.e("checkTime","currentTime="+currentTime);
+                Log.e("checkTime", "currentTime=" + currentTime);
                 //获取创建时间
                 long buildTime = Long.valueOf(time);
-                Log.e("checkTime","buildTime="+buildTime);
+                Log.e("checkTime", "buildTime=" + buildTime);
                 //获取过期时间
                 long newOutTime = buildTime + outDay * DAY_TIME;
                 //获取最新的测试码时间
                 long[] newTime = getNewTime();
 
-                if(newTime != null){
+                if (newTime != null) {
                     //比较创建时间
                     //比较创建时间
-                    if(newTime[0] > buildTime){
+                    if (newTime[0] > buildTime) {
                         buildTime = newTime[0];
                     }
 
-                    if(newTime[1] > newOutTime){
+                    if (newTime[1] > newOutTime) {
                         newOutTime = newTime[1];
                     }
                 }
-                if(currentTime > newOutTime || currentTime < buildTime){
+                if (currentTime > newOutTime || currentTime < buildTime) {
                     //超过有效期
                     callback.method(true);
-                }else{
+                } else {
                     callback.method(false);
                 }
             }
         }).start();
     }
 
-    public interface TimeOutCallback{
+    public interface TimeOutCallback {
 
         void method(boolean t);
     }
